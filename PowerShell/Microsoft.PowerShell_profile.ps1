06dda7b876e94e7d7659c3be5317e949f8d71546
Wayne Crouch <30832388+masked-owl@users.noreply.github.com>
2022-06-10 16:33:30 +1000

    Add touch function to profile

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,4 @@
 Set-Alias -name l -value ls
 $Scripts="C:\Users\{UserID}\AppData\Local\Microsoft\WindowsApps\"
 Set-PSReadlineKeyHandler -Key Ctrl+d -Function DeleteCharOrExit
+function touch {New-Item -ItemType File -Name ($args[0])}
